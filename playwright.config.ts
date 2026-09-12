4d9b66bf5bce0c2b089b9993b86a06645b756d20
Jeguerra93 <jeguerra77@gmail.com>
2026-09-11 19:36:12 -0500

    Adición de trace viewer

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,14 +34,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['json']],
+  reporter: [['html'], ['list', { printSteps: true }], ['json']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
      baseURL: process.env.QA_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+   // trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
